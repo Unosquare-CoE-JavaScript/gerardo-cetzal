7d661e6b28cfaf4666a6e1034c62e1a3966d873d
Gerardo Cetzal <gerardo.cetzal@unosquare.com>
2022-02-07 12:27:33 -0600

    Move project to own repository

---
+++ react-18/src/components/users/MovieDetails.tsx
@@ -0,0 +1,29 @@
+import useSWR from 'swr';
+import type { Movie } from '../../types';
+// import { Loading } from '../Loading';
+import { LabelInput } from './LabelInput';
+import { LabelTextarea } from './LabelTextarea';
+
+interface Props {
+  movieId: number;
+}
+export function MovieDetails({ movieId }: Props) {
+  const { data } = useSWR<Movie, Error>(
+    `${process.env.REACT_APP_API_BASE}/top-rated-movies/${movieId}?sleep=500`
+  );
+
+  const movie = data!;
+
+  return (
+    <div className="row">
+      <LabelInput label="Title" value={movie.title} readOnly />
+      <LabelInput label="Release date" value={movie.release_date} readOnly />
+      <LabelTextarea
+        label="Overview"
+        value={movie.overview}
+        readOnly
+        rows={5}
+      />
+    </div>
+  );
+}
---
+++ react-18/src/components/users/UserDetails.tsx
@@ -0,0 +1,26 @@
+//import { Suspense } from 'react';
+//import { Loading } from '../Loading';
+import { AccountDetails } from './AccountDetails';
+import { MovieDetails } from './MovieDetails';
+
+interface Props {
+  userId: number;
+  movieId: number;
+  isPending: boolean;
+}
+export function UserDetails({ userId, movieId, isPending }: Props) {
+  return (
+    <div>
+        {/* <SuspenseList revealOrder="together"> */}
+          <h4 className="text-center mt-5">User details</h4>
+          {/* <Suspense fallback={<Loading />}> */}
+            <AccountDetails userId={userId} /> 
+          {/* </Suspense> */}
+          <h4 className="text-center mt-5">Favorite movie</h4>
+          {/* {/* <Suspense fallback={<Loading />}> */}
+            <MovieDetails movieId={movieId} />
+          {/* </Suspense> */}
+        {/*</SuspenseList> */}
+    </div>
+  );
+}
