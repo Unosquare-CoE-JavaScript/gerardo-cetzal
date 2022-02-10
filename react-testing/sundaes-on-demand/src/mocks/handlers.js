30d8543ee14b997253d0044cded9300b50b6b758
Gerardo Cetzal <gerardo.cetzal@unosquare.com>
2022-02-10 15:54:34 -0600

    Mock service worker setup

---
+++ react-testing/sundaes-on-demand/src/mocks/handlers.js
@@ -0,0 +1,12 @@
+import { rest } from 'msw';
+
+export const handlers = [
+    rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
+        return res(
+            ctx.json([
+                { name: 'Chocolate', imgPath: '/images/chocolate.png' },
+                { name: 'Vanilla', imgPath: '/images/vanilla.png' }
+            ])
+        )
+    }),
+];
