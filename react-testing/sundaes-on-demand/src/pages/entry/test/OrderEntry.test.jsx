da5be59058da2dbf8bcfc7e2552e4e72d6e791ee
Gerardo Cetzal <gerardo.cetzal@unosquare.com>
2022-02-10 22:19:25 -0600

    Removed warnings

--- react-testing/sundaes-on-demand/src/pages/entry/test/OrderEntry.test.jsx
+++ react-testing/sundaes-on-demand/src/pages/entry/test/OrderEntry.test.jsx
@@ -3,10 +3,15 @@ import OrderEntry from "../OrderEntry";
 import { rest } from 'msw';
 import { server } from '../../../mocks/server';
 
+// leave the curly braces and add an explicit return. Remove warnings
 test('handles error for scoops and toppings routes', async () => {
     server.resetHandlers(
-        rest.get('http://localhost:3030/scoops', (req, res, ctx) => res(ctx.status(500)),
-            rest.get('http://localhost:3030/toppings', (req, res, ctx) => res(ctx.status(500))))
+        rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
+            return res(ctx.status(500));
+        }),
+        rest.get('http://localhost:3030/toppings', (req, res, ctx) => {
+            return res(ctx.status(500));
+        })
     );
 
     render(<OrderEntry />);
